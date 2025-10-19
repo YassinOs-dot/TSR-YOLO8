47c31388beee36832217fa6e03d8af86f2167601
YassinOs-dot <yassineabdelwahed11@gmail.com>
2025-10-19 14:48:18 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Traffic Sign Recognition System - Launcher
+"""
+from main_gui import TrafficSignGUI
+
+if __name__ == "__main__":
+    app = TrafficSignGUI()
+    app.mainloop()
